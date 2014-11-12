d201f323f5b70427345a8f74a1b73e2a27cab95e
Rene Bentes Pinto <renebentes@yahoo.com.br>
2014-11-12 16:59:55 -0300

    first

--- index.php
+++ index.php
@@ -19,55 +19,38 @@ if ($cck->initialize() === false)
 // -- Prepare
 $items = $cck->getItems();
 $count = count($items);
-$id    = !empty($cck->id_class) ? trim($cck->id_class) : 'generic-slideshow';
+$id    = !empty($cck->id_class) ? trim($cck->id_class) : 'genericTab';
+$i     = 0;
+$positions = $cck->getPositions();
 
-if ($cck->getStyleParam('navigator') == 1)
-{
-	$navLeft  = 'glyphicon glyphicon-chevron-left';
-	$navRight = 'glyphicon glyphicon-chevron-right';
-}
-elseif ($cck->getStyleParam('navigator') == 2)
-{
-	$navLeft  = 'fa fa-chevron-left';
-	$navRight = 'fa fa-chevron-right';
-}
-elseif ($cck->getStyleParam('navigator') == 0)
-{
-	$navLeft  = 'icon-prev';
-	$navRight = 'icon-next';
-}
-else
-{
-  $navLeft  = '';
-  $navRight = '';
-}
 
 // -- Render
 if (!empty($items)) : ?>
-<div id="<?php echo $id; ?>" class="carousel<?php echo $cck->getStyleParam('event') == 1 ? ' slide' : null; ?>" data-ride="carousel">
-	<ol class="carousel-indicators">
-  <?php for($i = 0; $i < $count; $i++) : ?>
-    <li data-target="#<?php echo $id; ?>" data-slide-to="<?php echo $i ?>"<?php echo $i == 0 ? 'class="active"' : null;?>></li>
-  <?php endfor; ?>
-  </ol>
-	<div class="carousel-inner" role="listbox">
-	<?php foreach ($items as $key => $item) : ?>
-		<div class="item<?php echo $key == 0 ? ' active' : null; ?>">
-			<?php echo $cck->renderItem($key); ?>
-		</div>
-	<?php endforeach; ?>
-	</div>
-	<?php if($count > 1 && $cck->getStyleParam('navigator') != -1) : ?>
-  <a class="left carousel-control" href="#<?php echo $id; ?>" role="button" data-slide="prev">
-  	<span class="<?php echo $navLeft; ?>"></span>
-  	<span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#<?php echo $id; ?>" role="button" data-slide="next">
-  	<span class="<?php echo $navRight; ?>"></span>
-  	<span class="sr-only">Next</span>
-  </a>
-<?php endif; ?>
-</div>
+  <ul id="<?php echo $id; ?>" class="nav nav-tabs<?php echo $cck->getStyleParam('class') ? ' ' . trim($cck->getStyleParam('class')) : null; ?>" role="tablist">
+  <?php foreach ($items as $key => $item) : ?>
+    <li role="presentation"<?php echo $i == 0 ? ' class="active"' : null; ?>>
+      <a href="#<?php echo $key; ?>" id="<?php echo $key; ?>-tab" role="tab" data-toggle="tab" aria-controls="<?php echo $key; ?>" aria-expanded="<?php echo $i == 0 ? 'true' : false; ?>">
+      <?php $fieldnames = $cck->getFields('tab', '', false);
+      foreach ($fieldnames as $fieldname) : ?>
+        <?php echo $item->renderField($fieldname); ?>
+      <?php endforeach; ?>
+      </a>
+    </li>
+    <?php $i++;
+  endforeach; ?>
+  </ul>
+  <?php $i = 0; ?>
+	<div id="<?php echo $id; ?>Content" class="tab-content<?php echo $cck->getStyleParam('class_content') ? ' ' . trim($cck->getStyleParam('class_content')) : null; ?>">
+  <?php foreach ($items as $key => $item) : ?>
+    <div role="tabpanel" class="tab-pane fade<?php echo $i == 0 ? ' active in' : null; ?>" id="<?php echo $key; ?>" aria-labelledby="<?php echo $key; ?>-tab">
+    <?php $fieldnames = $cck->getFields('content', '', false);
+    foreach ($fieldnames as $fieldname) : ?>
+      <?php echo $item->renderField($fieldname); ?>
+    <?php endforeach; ?>
+    </div>
+    <?php $i++;
+  endforeach; ?>
+  </div>
 <?php endif;
 
 // -- Finalize
